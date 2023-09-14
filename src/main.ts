5abffead0d5d4990a59cae287e81fa4c78adb934
Thomas.L <144795071+tmslpm@users.noreply.github.com>
2023-09-14 10:14:17 +0000

    test typedoc

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,13 @@ import { Manager } from "./base/Manager";
 
 console.log("Main.ts start...");
 
+/**
+ * return string length
+ * 
+ * @param a string A 
+ * @param b string B
+ * @returns number, the size length
+ */
 function test(a: string, b: string): number {
     return (a + b).length;
 }

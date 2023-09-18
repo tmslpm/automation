7cbfb45ee4a4707b1cf591112e01925fb5327262
Thomas.L <144795071+tmslpm@users.noreply.github.com>
2023-09-18 15:01:52 +0000

    add test unit for function `getOrThrow(...)` and `getOrDefault(...)`

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -2,8 +2,8 @@ export function getOrDefault(value: string, defaultValue: string): string {
     return value && value.length > 0 ? value : defaultValue;
 };
 
-export function getOrThrow(value: string) {
-    if (value && value.length)
+export function getOrThrow(value: any) {
+    if (value != null || value != undefined)
         return value;
-    throw new Error("NullPointerException value:" + value);
+    throw new Error("NullPointerException value: " + value);
 };
--- test/fnUtils.test.ts
+++ test/fnUtils.test.ts
@@ -1,27 +1,60 @@
 import { expect, test } from '@jest/globals';
-import { getOrDefault } from '../src/utils/Utils';
+import { getOrDefault, getOrThrow } from '../src/utils/Utils';
 
-let FAKE_DATA = {
-    value: "string",
-    default_value: "default_String"
-};
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
 
-// [getOrDefault] Valid return value
-test(`getOrDefault(${FAKE_DATA.value}, ${FAKE_DATA.default_value}) return ${FAKE_DATA.value}`, () => {
-    expect(getOrDefault(FAKE_DATA.value, FAKE_DATA.default_value)).toBe(FAKE_DATA.value);
+// [getOrDefault] entry is string return entry
+test(`test getOrDefault(...): entry is string return entry`, () => {
+    let entry = "string"
+    expect(getOrDefault(entry, "default_value")).toBe(entry);
+    entry = " string "
+    expect(getOrDefault(entry, "default_value")).toBe(entry);
+    entry = "_-stri ng"
+    expect(getOrDefault(entry, "default_value")).toBe(entry);
+    entry = "str dqz zqing"
+    expect(getOrDefault(entry, "default_value")).toBe(entry);
+    entry = "244"
+    expect(getOrDefault(entry, "default_value")).toBe(entry);
 });
 
-// [getOrDefault] Invalid return default value
-test(`getOrDefault("", ${FAKE_DATA.default_value}) return ${FAKE_DATA.default_value}`, () => {
-    expect(getOrDefault("", FAKE_DATA.default_value)).toBe(FAKE_DATA.default_value);
+// [getOrDefault] entry is empty return default value
+test(`test getOrDefault(...): entry is empty return default value`, () => {
+    expect(getOrDefault("", "default_value")).toBe("default_value");
 });
 
-// [getOrDefault] Invalid return default value
-test(`getOrDefault(undefined, ${FAKE_DATA.default_value}) return ${FAKE_DATA.default_value}`, () => {
-    expect(getOrDefault(undefined as any, FAKE_DATA.default_value)).toBe(FAKE_DATA.default_value);
+// [getOrDefault] entry undefined|null return default value
+test(`test getOrDefault(...): entry is undefined|null return default value`, () => {
+    expect(getOrDefault(undefined as any, "default_value")).toBe("default_value");
+    expect(getOrDefault(null as any, "default_value")).toBe("default_value");
 });
 
-// [getOrDefault] Invalid return default value
-test(`getOrDefault(null, ${FAKE_DATA.default_value}) return ${FAKE_DATA.default_value}`, () => {
-    expect(getOrDefault(null as any, FAKE_DATA.default_value)).toBe(FAKE_DATA.default_value);
+// [getOrDefault] entry not string return default value
+test(`test getOrDefault(...): entry not string return default value`, () => {
+    expect(getOrDefault(0 as any, "default_value")).toBe("default_value");
+    expect(getOrDefault(155 as any, "default_value")).toBe("default_value");
+    expect(getOrDefault(true as any, "default_value")).toBe("default_value");
+    expect(getOrDefault(false as any, "default_value")).toBe("default_value");
+});
+
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+// [getOrThrow] entry has value return value
+test("test getOrThrow(...): entry has value return entry", () => {
+    let data = { id: "string", num: 0, has: true, hasNot: false };
+    Object.values(data).forEach(v => expect(getOrThrow(v)).toBe(v));
+});
+
+// [getOrThrow] entry has null|undefined throw error 
+test("test getOrThrow(...): entry has null|undefined throw error", () => {
+    // TEST: null
+    expect(() => getOrThrow(null)).toThrow(/NullPointerException/);
+    // TEST: undefined 
+    expect(() => getOrThrow(undefined)).toThrow(/NullPointerException/);
+    // TEST: EMPTY
+    let dataEmpty = {} as any;
+    expect(() => getOrThrow(dataEmpty.id)).toThrow(/NullPointerException/);
+    expect(() => getOrThrow(dataEmpty.undimdq)).toThrow(/NullPointerException/);
+    expect(() => getOrThrow(dataEmpty.hasNot)).toThrow(/NullPointerException/);
 });
